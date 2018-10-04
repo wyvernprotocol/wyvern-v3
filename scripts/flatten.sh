a5e1fdceb8f18497f2475d945a87344214438e90
Christopher Goes <cwgoes@pluranimity.org>
2018-10-04 14:41:02 +0200

    Solidity source flattening

---
+++ scripts/flatten.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -ex
+
+rm -rf temp
+mkdir -p temp
+
+alias flatten="yarn run truffle-flattener"
+
+flatten contracts/WyvernAtomicizer.sol --output temp/WyvernAtomicizer.sol
+flatten contracts/WyvernRegistry.sol --output temp/WyvernRegistry.sol
+flatten contracts/WyvernExchange.sol --output temp/WyvernExchange.sol
