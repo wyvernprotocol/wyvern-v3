5ae52460fc208c60affdcefcfeba3f0092c5607d
Christopher Goes <cwgoes@pluranimity.org>
2018-10-04 15:25:32 +0200

    Grant initial registry auth to exchange contract

--- migrations/4_wyvern_registry.js
+++ migrations/4_wyvern_registry.js
@@ -1,10 +1,14 @@
 /* global artifacts:false */
 
 const WyvernRegistry = artifacts.require('./WyvernRegistry.sol')
+const WyvernExchange = artifacts.require('./WyvernExchange.sol')
 const { setConfig } = require('./config.js')
 
 module.exports = (deployer, network) => {
   return deployer.deploy(WyvernRegistry).then(() => {
     if (network !== 'development') setConfig('deployed.' + network + '.WyvernRegistry', WyvernRegistry.address)
+    return WyvernRegistry.deployed().then(registry => {
+      return registry.grantInitialAuthentication(WyvernExchange.address)
+    })
   })
 }
