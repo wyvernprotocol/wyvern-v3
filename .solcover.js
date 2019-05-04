f21e7fb0c4a0cc1b57d82e32da90d98ba6037c2b
Christopher Goes <cwgoes@pluranimity.org>
2019-05-04 18:41:34 +0200

    Attempt to fix solcover; little luck

--- .solcover.js
+++ .solcover.js
@@ -2,17 +2,13 @@ module.exports = {
   port: 8545,
   copyPackages: ['openzeppelin-solidity'],
   skipFiles: [
-    'lib/StaticCaller.sol',
-    'lib/ArrayUtils.sol',
-    'static/StaticUtil.sol',
-    'static/StaticERC20.sol',
-    'static/StaticERC721.sol',
-    'static/StaticCompat.sol',
-    'static/StaticPayment.sol',
+    'static/',
+    'lib/',
     'TestERC20.sol',
     'TestERC721.sol',
     'TestAuthenticatedProxy.sol',
-    'WyvernAtomicizer.sol'
+    'WyvernAtomicizer.sol',
+    'WyvernStatic.sol'
   ],
   compileCommand: '../node_modules/.bin/truffle compile',
   testCommand: '../node_modules/.bin/truffle test --network coverage'
--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rm -rf build
+rm -rf build temp
 ./node_modules/.bin/solidity-coverage
 cat coverage/lcov.info | ./node_modules/.bin/coveralls
