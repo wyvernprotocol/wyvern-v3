9ea1fd466a62952c80d5bab5282e499009e418f3
Christopher Goes <cwgoes@pluranimity.org>
2019-05-31 21:18:24 +0200

    Try coverage

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,5 +4,24 @@ set -xe
 
 rm -rf build temp
 
+echo "Removing 'view' modifier..."
+
+find ./contracts -type f -name "*.sol" -exec sed -i -e 's/view//g' {} +;
+
+echo "Building ethereumjs-testrpc-sc..."
+
+pushd node_modules/solidity-coverage
+npm i
+pushd node_modules/ethereumjs-testrpc-sc
+npm i
+npm run-script build
+popd
+popd
+
+echo "Running solidity-coverage..."
+
 ./node_modules/.bin/solidity-coverage
+
+echo "Uploading coverage information..."
+
 cat coverage/lcov.info | ./node_modules/.bin/coveralls
