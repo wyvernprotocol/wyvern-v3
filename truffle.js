ac57083bc9ebf8218ec31e07892138f9cbdef3d8
MarvinJanssen <MarvinJanssen@users.noreply.github.com>
2021-04-02 10:13:11 -0700

    Klaytn configs

--- truffle.js
+++ truffle.js
@@ -1,8 +1,19 @@
-var HDWalletProvider = require('truffle-hdwallet-provider')
+require('dotenv').config()
 
-var rinkebyMnemonic = ''
-var mumbaiMnemonic = ''
-var mainnetMnemonic = ''
+var HDWalletProvider = require('truffle-hdwallet-provider')
+var KlaytnHDWalletProvider = require('truffle-hdwallet-provider-klaytn')
+var Caver = require('caver-js')
+
+var rinkebyMnemonic = process.env.RINKEBY_MNEMONIC || ''
+var mumbaiMnemonic = process.env.MUMBAI_MNEMONIC || ''
+var mainnetMnemonic = process.env.MAINNET_MNEMONIC || ''
+var klaytnPrivateKey = process.env.KLAYTN_PRIVATE_KEY || ''
+var baobabPrivateKey = process.env.BAOBAB_PRIVATE_KEY || ''
+var infuraKey = process.env.INFURA_KEY || '';
+
+var kasAccessKeyId = process.env.KAS_ACCESS_KEY_ID || ''
+var kasSecretAccessKey = process.env.KAS_SECRET_KEY || ''
+
 
 module.exports = {
   mocha: {
@@ -34,12 +45,13 @@ module.exports = {
     },
     rinkeby: {
       provider: function () {
-        return new HDWalletProvider(rinkebyMnemonic, 'https://rinkeby.infura.io')
+        return new HDWalletProvider(rinkebyMnemonic, 'https://rinkeby.infura.io/v3/'+infuraKey)
       },
       from: '',
       port: 8545,
       network_id: '4',
       gas: 6700000,
+      networkCheckTimeout: 100000,
       gasPrice: 21110000000,
       confirmations: 2
     },
@@ -49,7 +61,41 @@ module.exports = {
       },
       from: '',
       network_id: '80001'
-      }
+    },
+    baobab: {
+      provider: () => {
+        const options = {
+          headers: [
+            { name: 'Authorization', value: 'Basic ' + Buffer.from(kasAccessKeyId + ':' + kasSecretAccessKey).toString('base64') },
+            { name: 'x-chain-id', value: '1001' }
+          ],
+          keepAlive: false,
+        }
+        return new KlaytnHDWalletProvider(baobabPrivateKey, new Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn", options))
+      },
+      from: '',
+      network_id: '1001',
+      networkCheckTimeout: 10000,
+      gas: '8500000',
+      gasPrice:'25000000000'
+    },
+    klaytn: {
+      provider: () => {
+        const options = {
+          headers: [
+            { name: 'Authorization', value: 'Basic ' + Buffer.from(kasAccessKeyId + ':' + kasSecretAccessKey).toString('base64') },
+            { name: 'x-chain-id', value: '8217' }
+          ],
+          keepAlive: false,
+        }
+        return new KlaytnHDWalletProvider(klaytnPrivateKey, new Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn", options))
+      },
+      from: '',
+      network_id: '8217',
+      networkCheckTimeout: 10000,
+      gas: '8500000',
+      gasPrice:'25000000000'
+    }
   },
   compilers: {
     solc: {
